eaf89ebe7830a7f781abb17dda97aeee1aad2885
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2018-12-17 20:07:00 +0600

    meals add, update, list updated

--- meals/views.py
+++ meals/views.py
@@ -1,31 +1,40 @@
 import datetime
 
-from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, ListView, UpdateView
 
 from meals.forms import AddMealForm, UpdateMealForm
 from meals.models import Meal
 
 
-@method_decorator([login_required(login_url='/admin/login/')], name='dispatch')
-class MealCreateView(CreateView):
+class MealCreateView(LoginRequiredMixin, CreateView):
     template_name = 'meals/add.html'
     form_class = AddMealForm
+    login_url = '/accounts/login/'
 
     def form_valid(self, form):
+        is_added_meal = Meal.objects.filter(meal_date__day=datetime.datetime.today().day, user_id=self.request.user.pk)
         meal = form.save(commit=False)
         meal.user_id = self.request.user.pk
         meal.save()
+        messages.success(self.request, 'Meal added successfully.')
         return HttpResponseRedirect(reverse('meal-add'))
 
+    def dispatch(self, *args, **kwargs):
+        is_added_meal = Meal.objects.filter(meal_date__day=datetime.datetime.today().day, user_id=self.request.user.pk)
+        if is_added_meal:
+            messages.error(self.request, 'Today meals already added.')
+            return HttpResponseRedirect(reverse('meal-list'))
 
-@method_decorator([login_required(login_url='/admin/login/')], name='dispatch')
-class MealListView(ListView):
+        return super().dispatch(*args, **kwargs)
+
+
+class MealListView(LoginRequiredMixin, ListView):
     template_name = 'meals/list.html'
-
+    login_url = '/accounts/login/'
     model = Meal
 
     # paginate_by = 1  # if pagination is desired
@@ -33,25 +42,22 @@ class MealListView(ListView):
     def get_context_data(self, **kwargs):
         current_month = datetime.datetime.now().month
         context = super().get_context_data(**kwargs)
-        context['object_list'] = Meal.objects.filter(user_id=self.request.user.pk, meal_date__month=current_month).order_by(
+        context['object_list'] = Meal.objects.filter(user_id=self.request.user.pk,
+                                                     meal_date__month=current_month).order_by(
             'meal_date')
+        context['meal_count'] = Meal.objects.count()
         return context
 
 
-@method_decorator([login_required(login_url='/admin/login/')], name='dispatch')
-class MealUpdateView(UpdateView):
+class MealUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'meals/update.html'
     model = Meal
     form_class = UpdateMealForm
-    success_message = 'A project updated successfully.'
+    login_url = '/accounts/login/'
 
     def form_valid(self, form):
         meal = form.save(commit=False)
         meal.user_id = self.request.user.pk
         meal.save()
+        messages.success(self.request, 'A meal updated successfully.')
         return HttpResponseRedirect(reverse('meal-update', kwargs={'pk': meal.pk}))
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-
