96594243f021552b8261d69703a3d04c5dc454eb
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2018-12-20 21:33:33 +0600

    dashboard meal, bazaar, extra cost show for current month and previous month with corresponding member

--- core/views/dashboard.py
+++ core/views/dashboard.py
@@ -27,23 +27,43 @@ class DashboardView(LoginRequiredMixin, View):
 
         current_month_total_meals = Meal.objects.filter(meal_date__month=current_month) \
             .aggregate(total_meals=Sum(F('breakfast') + F('lunch') + F('dinner')))
+        current_month_total_bazaar = Bazaar.objects.filter(bazaar_date__month=current_month) \
+            .aggregate(total_bazaar=Sum('item_price'))
+        current_month_total_extra_cost = ExtraCost.objects.filter(expense_date__month=current_month) \
+            .aggregate(total_extra_costs=Sum('cost'))
+
+        previous_month_total_meals = Meal.objects.filter(meal_date__month=previous_month.month) \
+            .aggregate(total_meals=Sum(F('breakfast') + F('lunch') + F('dinner')))
+        previous_month_total_bazaar = Bazaar.objects.filter(bazaar_date__month=previous_month.month) \
+            .aggregate(total_bazaar=Sum('item_price'))
+        previous_month_total_extra_cost = ExtraCost.objects.filter(expense_date__month=previous_month.month) \
+            .aggregate(total_extra_costs=Sum('cost'))
+
         current_month_user_meals = Meal.objects.select_related('user') \
             .filter(meal_date__month=current_month) \
             .values('user_id', 'user__first_name', 'user__last_name') \
             .annotate(total_meals=Sum(F('breakfast') + F('lunch') + F('dinner')))
+        current_month_user_bazaar = Bazaar.objects.select_related('user') \
+            .filter(bazaar_date__month=current_month) \
+            .values('user_id', 'user__first_name', 'user__last_name') \
+            .annotate(total_bazaar=Sum('item_price'))
+        current_month_user_extra_cost = ExtraCost.objects.select_related('user') \
+            .filter(expense_date__month=current_month) \
+            .values('user_id', 'user__first_name', 'user__last_name') \
+            .annotate(total_extra_costs=Sum('cost'))
 
-        previous_month_total_meals = Meal.objects.filter(meal_date__month=previous_month.month) \
-            .aggregate(total_meals=Sum(F('breakfast') + F('lunch') + F('dinner')))
-
-        current_month_total_bazaar = Bazaar.objects.filter(bazaar_date__month=current_month) \
-            .aggregate(total_bazaar=Sum('item_price'))
-        previous_month_total_bazaar = Bazaar.objects.filter(bazaar_date__month=previous_month.month) \
-            .aggregate(total_bazaar=Sum('item_price'))
-
-        current_month_total_extra_cost = ExtraCost.objects.filter(expense_date__month=current_month) \
-            .aggregate(total_extra_costs=Sum('cost'))
-        previous_month_total_extra_cost = ExtraCost.objects.filter(expense_date__month=previous_month.month) \
-            .aggregate(total_extra_costs=Sum('cost'))
+        previous_month_user_meals = Meal.objects.select_related('user') \
+            .filter(meal_date__month=previous_month.month) \
+            .values('user_id', 'user__first_name', 'user__last_name') \
+            .annotate(total_meals=Sum(F('breakfast') + F('lunch') + F('dinner')))
+        previous_month_user_bazaar = Bazaar.objects.select_related('user') \
+            .filter(bazaar_date__month=previous_month.month) \
+            .values('user_id', 'user__first_name', 'user__last_name') \
+            .annotate(total_bazaar=Sum('item_price'))
+        previous_month_user_extra_cost = ExtraCost.objects.select_related('user') \
+            .filter(expense_date__month=previous_month.month) \
+            .values('user_id', 'user__first_name', 'user__last_name') \
+            .annotate(total_extra_costs=Sum('cost'))
 
         context = {
             'today_meals_count': today_meals_count,
@@ -51,9 +71,15 @@ class DashboardView(LoginRequiredMixin, View):
             'current_month_total_meals': current_month_total_meals,
             'current_month_user_meals': current_month_user_meals,
             'current_month_total_bazaar': current_month_total_bazaar,
+            'current_month_user_bazaar': current_month_user_bazaar,
             'current_month_total_extra_cost': current_month_total_extra_cost,
+            'current_month_user_extra_cost': current_month_user_extra_cost,
             'previous_month_total_meals': previous_month_total_meals,
             'previous_month_total_bazaar': previous_month_total_bazaar,
-            'previous_month_total_extra_cost': previous_month_total_extra_cost
+            'previous_month_total_extra_cost': previous_month_total_extra_cost,
+            'previous_month_user_meals': previous_month_user_meals,
+            'previous_month_user_bazaar': previous_month_user_bazaar,
+            'previous_month_user_extra_cost': previous_month_user_extra_cost,
+            'previous_month': previous_month
         }
         return render(request, self.template_name, context)
